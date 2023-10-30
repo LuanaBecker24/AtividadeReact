9efecacbb720787b710023caf451bdf534cdb639
LuanaBecker24 <130504315+LuanaBecker24@users.noreply.github.com>
2023-10-30 20:33:36 -0300

    terminando

--- React/src/pages/Home/index.jsx
+++ React/src/pages/Home/index.jsx
@@ -1,5 +1,7 @@
 import Card from "../../components/Card"
 
+import { ArrowRight, ChevronRight, Minus } from 'lucide-react'
+
 import "./home.css";
 
 export default function Home() {
@@ -65,45 +67,45 @@ export default function Home() {
               precoAtual={"Rp 2.500.000"}
               precoAntigo={"Rp 3.500.000"}
             />
-            <Card 
+            <Card
               imagem={"../../public/images/image 2.png"}
               titulo={"Leviosa"}
               descricao={"Stylish cafe chair"}
               precoAtual={"Rp 2.500.000"}
             />
-            <Card 
+            <Card
               imagem={"../../public/images/images (1).png"}
               titulo={"Lolito"}
               descricao={"Luxury big sofa"}
               precoAtual={"Rp 7.000.000"}
               precoAntigo={"Rp 14.000.000"}
             />
-            <Card 
+            <Card
               imagem={"../../public/images/images (2).png"}
               titulo={"Respira"}
               descricao={"Outdoor bar table and stool"}
               precoAtual={"Rp 500.000"}
             />
-            <Card 
+            <Card
               imagem={"../../public/images/images (3).png"}
               titulo={"Grifo"}
               descricao={"Night lamp"}
               precoAtual={"Rp 1.500.000"}
             />
-            <Card 
+            <Card
               imagem={"../../public/images/images (4).png"}
               titulo={"Muggo"}
               descricao={"Small mug"}
               precoAtual={"Rp 150.000"}
             />
-            <Card 
+            <Card
               imagem={"../../public/images/images (5).png"}
               titulo={"Pingky"}
               descricao={"Cute bed set"}
               precoAtual={"Rp 7.000.000"}
               precoAntigo={"Rp 14.000.000"}
             />
-            <Card 
+            <Card
               imagem={"../../public/images/image 7.png"}
               titulo={"Potty"}
               descricao={"Minimalist flower pot"}
@@ -113,6 +115,41 @@ export default function Home() {
           </div>
           <div className="botao_veja_mais">
             <a>Show More</a>
+          </div>
+        </div>
+        <div className="quarta_parte">
+          <div className="parte_inicial">
+            <div className="escritas">
+              <h1>50+ Beautiful rooms inspiration</h1>
+              <p>Our designer already made a lot of beautiful prototipe of rooms that inspire you</p>
+              <div className="botao_explorar">
+                <a>Explore More</a>
+              </div>
+            </div>
+            <div className="segundo_banner">
+              <div className="container_elementos">
+                <div className="conteudo_total">
+                  <div className="escrita_total">
+                    <div className="primeira_escrita">
+                      <p>01</p>
+                      <Minus />
+                      <p>Bed Room</p>
+                    </div>
+                    <h2>Inner Peace</h2>
+                  </div>
+                  <div className="botao_proximo">
+                    <a><ArrowRight /></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="carrossel_imagens">
+            <img src="../../public/images/Rectangle 25.png" alt="primeira_imagem_carrossel" className="primeira_imagem_carrossel" />
+            <div className="botao_passar">
+              <a><ChevronRight /></a>
+            </div>
+            <img src="../../public/images/Rectangle 26.png" alt="segunda_imagem_carrossel" className="segunda_imagem_carrossel" />
           </div>
         </div>
       </main>
